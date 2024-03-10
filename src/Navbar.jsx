e8aa1c68598edf9d96a3858f24901880a196ca27
somesh202 <somesh.ranjan.met20@itbhu.ac.in>
2024-03-10 21:43:05 +0530

    minor fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className='navbar'>
       <div className='nav-header'>
-        <h1 className='story-header'>SYSU</h1>
+        <h1 className='story-header'>SYSU </h1>
         <h4>Journal in Public</h4>
       </div>
     </div>
